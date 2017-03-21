152d06d5e3c37c771e31321151a398adae2b0cdd
Alemat Gebru <alematgebru@gmail.com>
2017-03-21 04:47:27 -0700

    Update Simprint.js

--- www/Simprint.js
+++ www/Simprint.js
@@ -2,6 +2,10 @@
 
 //  This is (Simprint.js) what gets called first and then kicks off Cordova to call our native code. */ 
 
+// Let's start with the JavaScript API that we’d like to expose to the developer.
+// In this case, our JavaScript code lives in www/Simprint.js.
+
+
 
 /*var exec = require('cordova/exec');
 
@@ -12,7 +16,8 @@ var Simprint = {
     exec(cb, null, PLUGIN_NAME, 'echo', [phrase]);
   }
 
- 
+ :
+ :
   
 };
 
